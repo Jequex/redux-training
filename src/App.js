2275acfd70077e7637b4a3f57bebecd4d0c676bd
jequex <onyejekwe.harddisk20@gmail.com>
2021-07-30 22:18:35 +0100

    ok for now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import TaskList from './components/TaskList';
+import User from './components/User';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
+
+  const add = () => {
+    console.log("adding....");
+  };
+
+  const deletes = () => {
+    console.log("deleting...");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <User />
+        <TaskList />
+        <br />
+        <button onClick={add}>Add Task</button>{" "}
+        <button onClick={deletes}>Delete Task</button>
+      </div>
+    </Provider>
   );
 }
 
